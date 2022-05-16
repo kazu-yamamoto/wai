b635ef49c242116f6f06e4c6e8d0fc83bdc870b2
Kazu Yamamoto <kazu@iij.ad.jp>
2022-05-16 10:41:04 +0900

    dejafu for auto-update-0.1.6

--- auto-update/Control/AutoUpdateDejaFu.hs
+++ auto-update/Control/AutoUpdateDejaFu.hs
@@ -1,19 +1,39 @@
 {-# LANGUAGE ScopedTypeVariables #-}
-
--- | A common problem is the desire to have an action run at a scheduled
--- interval, but only if it is needed. For example, instead of having
--- every web request result in a new @getCurrentTime@ call, we'd like to
--- have a single worker thread run every second, updating an @IORef@.
--- However, if the request frequency is less than once per second, this is
--- a pessimization, and worse, kills idle GC.
+-- | In a multithreaded environment, running actions on a regularly scheduled
+-- background thread can dramatically improve performance.
+-- For example, web servers need to return the current time with each HTTP response.
+-- For a high-volume server, it's much faster for a dedicated thread to run every
+-- second, and write the current time to a shared 'IORef', than it is for each
+-- request to make its own call to 'getCurrentTime'.
 --
--- This library allows you to define actions which will either be
--- performed by a dedicated thread or, in times of low volume, will be
--- executed by the calling thread.
+-- But for a low-volume server, whose request frequency is less than once per 
+-- second, that approach will result in /more/ calls to 'getCurrentTime' than 
+-- necessary, and worse, kills idle GC.
+--
+-- This library solves that problem by allowing you to define actions which will
+-- either be performed by a dedicated thread, or, in times of low volume, will 
+-- be executed by the calling thread.
+--
+-- Example usage:
+--
+-- @
+-- import "Data.Time"
+-- import "Control.AutoUpdate"
+--
+-- getTime <- 'mkAutoUpdate' 'defaultUpdateSettings'
+--              { 'updateAction' = 'Data.Time.Clock.getCurrentTime'
+--              , 'updateFreq' = 1000000 -- The default frequency, once per second
+--              }
+-- currentTime <- getTime
+-- @
+--
+-- For more examples, <http://www.yesodweb.com/blog/2014/08/announcing-auto-update see the blog post introducing this library>.
+
 module Main where
 
 import           Control.Exception        (SomeException)
 import           Control.Monad
+import           Control.Monad.Catch (try)
 import           Control.Monad.Conc.Class
 
 import Test.DejaFu
@@ -36,11 +56,9 @@ nondeterministic = do
   void auto
   auto
 
--------------------------------------------------------------------------------
-
--- | Default value for creating an @UpdateSettings@.
+-- | Default value for creating an 'UpdateSettings'.
 --
--- Since 0.1.0
+-- @since 0.1.0
 defaultUpdateSettings :: MonadConc m => UpdateSettings m ()
 defaultUpdateSettings = UpdateSettings
     { updateFreq = 1000000
@@ -50,90 +68,137 @@ defaultUpdateSettings = UpdateSettings
 
 -- | Settings to control how values are updated.
 --
--- This should be constructed using @defaultUpdateSettings@ and record
+-- This should be constructed using 'defaultUpdateSettings' and record
 -- update syntax, e.g.:
 --
 -- @
--- let set = defaultUpdateSettings { updateAction = getCurrentTime }
+-- let settings = 'defaultUpdateSettings' { 'updateAction' = 'Data.Time.Clock.getCurrentTime' }
 -- @
 --
--- Since 0.1.0
+-- @since 0.1.0
 data UpdateSettings m a = UpdateSettings
     { updateFreq           :: Int
     -- ^ Microseconds between update calls. Same considerations as
-    -- @threadDelay@ apply.
+    -- 'threadDelay' apply.
     --
     -- Default: 1 second (1000000)
     --
-    -- Since 0.1.0
+    -- @since 0.1.0
     , updateSpawnThreshold :: Int
     -- ^ NOTE: This value no longer has any effect, since worker threads are
     -- dedicated instead of spawned on demand.
     --
-    -- Previously, this determined: How many times the data must be requested
+    -- Previously, this determined how many times the data must be requested
     -- before we decide to spawn a dedicated thread.
     --
     -- Default: 3
     --
-    -- Since 0.1.0
+    -- @since 0.1.0
     , updateAction         :: m a
     -- ^ Action to be performed to get the current value.
     --
     -- Default: does nothing.
     --
-    -- Since 0.1.0
+    -- @since 0.1.0
     }
 
 -- | Generate an action which will either read from an automatically
 -- updated value, or run the update action in the current thread.
 --
--- Since 0.1.0
+-- @since 0.1.0
 mkAutoUpdate :: MonadConc m => UpdateSettings m a -> m (m a)
-mkAutoUpdate us = do
-    -- The current value, if available.
-    currRef <- newIORef Nothing
+mkAutoUpdate us = mkAutoUpdateHelper us Nothing
 
+-- | Generate an action which will either read from an automatically
+-- updated value, or run the update action in the current thread if
+-- the first time or the provided modify action after that.
+--
+-- @since 0.1.4
+mkAutoUpdateWithModify :: MonadConc m => UpdateSettings m a -> (a -> m a) -> m (m a)
+mkAutoUpdateWithModify us f = mkAutoUpdateHelper us (Just f)
+
+mkAutoUpdateHelper :: forall m a . MonadConc m => UpdateSettings m a -> Maybe (a -> m a) -> m (m a)
+mkAutoUpdateHelper us updateActionModify = do
     -- A baton to tell the worker thread to generate a new value.
     needsRunning <- newEmptyMVar
 
-    -- The last value generated, to allow for blocking semantics when currRef
-    -- is Nothing.
-    lastValue <- newEmptyMVar
+    -- The initial response variable. Response variables allow the requesting
+    -- thread to block until a value is generated by the worker thread.
+    responseVar0 <- newEmptyMVar
 
-    -- fork the worker thread immediately...
-    void $ fork $ forever $ do
-        -- but block until a value is actually needed
-        takeMVar needsRunning
+    -- The current value, if available. We start off with a Left value
+    -- indicating no value is available, and the above-created responseVar0 to
+    -- give a variable to block on.
+    currRef <- newIORef $ Left responseVar0
 
-        -- new value requested, so run the updateAction
-        a <- catchSome $ updateAction us
+    -- This is used to set a value in the currRef variable when the worker
+    -- thread exits. In reality, that value should never be used, since the
+    -- worker thread exiting only occurs if an async exception is thrown, which
+    -- should only occur if there are no references to needsRunning left.
+    -- However, this handler will make error messages much clearer if there's a
+    -- bug in the implementation.
+    let fillRefOnExit f = do
+            eres <- try f
+            case eres of
+                Left e -> writeIORef currRef $ error $
+                    "Control.AutoUpdate.mkAutoUpdate: worker thread exited with exception: "
+                    ++ show (e :: SomeException)
+                Right () -> writeIORef currRef $ error $
+                    "Control.AutoUpdate.mkAutoUpdate: worker thread exited normally, "
+                    ++ "which should be impossible due to usage of infinite loop"
 
-        -- we got a new value, update currRef and lastValue
-        writeIORef currRef $ Just a
-        void $ tryTakeMVar lastValue
-        putMVar lastValue a
+    -- fork the worker thread immediately. Note that we mask async exceptions,
+    -- but *not* in an uninterruptible manner. This will allow a
+    -- BlockedIndefinitelyOnMVar exception to still be thrown, which will take
+    -- down this thread when all references to the returned function are
+    -- garbage collected, and therefore there is no thread that can fill the
+    -- needsRunning MVar.
+    --
+    -- Note that since we throw away the ThreadId of this new thread and never
+    -- calls myThreadId, normal async exceptions can never be thrown to it,
+    -- only RTS exceptions.
+    mask_ $ void $ fork $ fillRefOnExit $ do
+        -- This infinite loop makes up out worker thread. It takes an a
+        -- responseVar value where the next value should be putMVar'ed to for
+        -- the benefit of any requesters currently blocked on it.
+        let loop responseVar maybea = do
+                -- block until a value is actually needed
+                takeMVar needsRunning
 
-        -- delay until we're needed again
-        threadDelay $ updateFreq us
+                -- new value requested, so run the updateAction
+                a <- maybe (updateAction us) id (updateActionModify <*> maybea)
 
-        -- delay's over, clear out currRef and lastValue so that demanding the
-        -- value again forces us to start work
-        writeIORef currRef Nothing
-        void $ takeMVar lastValue
+                -- we got a new value, update currRef and lastValue
+                writeIORef currRef $ Right a
+                putMVar responseVar a
+
+                -- delay until we're needed again
+                threadDelay $ updateFreq us
+
+                -- delay's over. create a new response variable and set currRef
+                -- to use it, so that the next requester will block on that
+                -- variable. Then loop again with the updated response
+                -- variable.
+                responseVar' <- newEmptyMVar
+                writeIORef currRef $ Left responseVar'
+                loop responseVar' (Just a)
+
+        -- Kick off the loop, with the initial responseVar0 variable.
+        loop responseVar0 Nothing
 
     return $ do
         mval <- readIORef currRef
         case mval of
-            -- we have a current value, use it
-            Just val -> return val
-            Nothing -> do
+            Left responseVar -> do
                 -- no current value, force the worker thread to run...
                 void $ tryPutMVar needsRunning ()
 
                 -- and block for the result from the worker
-                readMVar lastValue
+                readMVar responseVar
+            -- we have a current value, use it
+            Right val -> return val
 
--- | Turn a runtime exception into an impure exception, so that all @IO@
+-- | Turn a runtime exception into an impure exception, so that all 'IO'
 -- actions will complete successfully. This simply defers the exception until
 -- the value is forced.
 catchSome :: MonadConc m => m a -> m a
