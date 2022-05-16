041fa16e12baaca61ae3ec531924552c2fc9d577
Kazu Yamamoto <kazu@iij.ad.jp>
2022-05-16 10:00:11 +0900

    sync with auto-update-0.1.1.4 for minimum diff

--- auto-update/Control/AutoUpdateDejaFu.hs
+++ auto-update/Control/AutoUpdateDejaFu.hs
@@ -1,41 +1,15 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
-{-
-The auto-update package:
-https://hackage.haskell.org/package/auto-update
-
-Users found a possible deadlock and livelock:
-https://www.reddit.com/r/haskell/comments/2i5d7m/updating_autoupdate/
-
-This is the code from Control.AutoUpdate modified to use the
-@MonadConc@ abstraction, with tests added to verify that the issues
-identified are caught.. The original code is available under the MIT
-license, which is reproduced below.
-
-- - - - -
-
-Copyright (c) 2014 Michael Snoyman
-
-Permission is hereby granted, free of charge, to any person obtaining
-a copy of this software and associated documentation files (the
-"Software"), to deal in the Software without restriction, including
-without limitation the rights to use, copy, modify, merge, publish,
-distribute, sublicense, and/or sell copies of the Software, and to
-permit persons to whom the Software is furnished to do so, subject to
-the following conditions:
-
-The above copyright notice and this permission notice shall be included
-in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--}
-
+-- | A common problem is the desire to have an action run at a scheduled
+-- interval, but only if it is needed. For example, instead of having
+-- every web request result in a new @getCurrentTime@ call, we'd like to
+-- have a single worker thread run every second, updating an @IORef@.
+-- However, if the request frequency is less than once per second, this is
+-- a pessimization, and worse, kills idle GC.
+--
+-- This library allows you to define actions which will either be
+-- performed by a dedicated thread or, in times of low volume, will be
+-- executed by the calling thread.
 module Main where
 
 import           Control.Exception        (SomeException)
@@ -64,47 +38,104 @@ nondeterministic = do
 
 -------------------------------------------------------------------------------
 
-data UpdateSettings m a = UpdateSettings
-    { updateFreq           :: Int
-    , updateSpawnThreshold :: Int
-    , updateAction         :: m a
-    }
-
+-- | Default value for creating an @UpdateSettings@.
+--
+-- Since 0.1.0
 defaultUpdateSettings :: MonadConc m => UpdateSettings m ()
 defaultUpdateSettings = UpdateSettings
-    { updateFreq           = 1000000
+    { updateFreq = 1000000
     , updateSpawnThreshold = 3
-    , updateAction         = pure ()
+    , updateAction = return ()
     }
 
+-- | Settings to control how values are updated.
+--
+-- This should be constructed using @defaultUpdateSettings@ and record
+-- update syntax, e.g.:
+--
+-- @
+-- let set = defaultUpdateSettings { updateAction = getCurrentTime }
+-- @
+--
+-- Since 0.1.0
+data UpdateSettings m a = UpdateSettings
+    { updateFreq           :: Int
+    -- ^ Microseconds between update calls. Same considerations as
+    -- @threadDelay@ apply.
+    --
+    -- Default: 1 second (1000000)
+    --
+    -- Since 0.1.0
+    , updateSpawnThreshold :: Int
+    -- ^ NOTE: This value no longer has any effect, since worker threads are
+    -- dedicated instead of spawned on demand.
+    --
+    -- Previously, this determined: How many times the data must be requested
+    -- before we decide to spawn a dedicated thread.
+    --
+    -- Default: 3
+    --
+    -- Since 0.1.0
+    , updateAction         :: m a
+    -- ^ Action to be performed to get the current value.
+    --
+    -- Default: does nothing.
+    --
+    -- Since 0.1.0
+    }
+
+-- | Generate an action which will either read from an automatically
+-- updated value, or run the update action in the current thread.
+--
+-- Since 0.1.0
 mkAutoUpdate :: MonadConc m => UpdateSettings m a -> m (m a)
 mkAutoUpdate us = do
-    currRef      <- newIORef Nothing
-    needsRunning <- newEmptyMVar
-    lastValue    <- newEmptyMVar
+    -- The current value, if available.
+    currRef <- newIORef Nothing
 
+    -- A baton to tell the worker thread to generate a new value.
+    needsRunning <- newEmptyMVar
+
+    -- The last value generated, to allow for blocking semantics when currRef
+    -- is Nothing.
+    lastValue <- newEmptyMVar
+
+    -- fork the worker thread immediately...
     void $ fork $ forever $ do
+        -- but block until a value is actually needed
         takeMVar needsRunning
 
+        -- new value requested, so run the updateAction
         a <- catchSome $ updateAction us
 
+        -- we got a new value, update currRef and lastValue
         writeIORef currRef $ Just a
         void $ tryTakeMVar lastValue
         putMVar lastValue a
 
+        -- delay until we're needed again
         threadDelay $ updateFreq us
 
+        -- delay's over, clear out currRef and lastValue so that demanding the
+        -- value again forces us to start work
         writeIORef currRef Nothing
         void $ takeMVar lastValue
 
-    pure $ do
+    return $ do
         mval <- readIORef currRef
         case mval of
-            Just val -> pure val
+            -- we have a current value, use it
+            Just val -> return val
             Nothing -> do
+                -- no current value, force the worker thread to run...
                 void $ tryPutMVar needsRunning ()
+
+                -- and block for the result from the worker
                 readMVar lastValue
 
+-- | Turn a runtime exception into an impure exception, so that all @IO@
+-- actions will complete successfully. This simply defers the exception until
+-- the value is forced.
 catchSome :: MonadConc m => m a -> m a
 catchSome act = catch act $
   \e -> throw (e :: SomeException)
