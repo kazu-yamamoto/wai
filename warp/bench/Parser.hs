c0880fa09f018eca025b7cb1db0369185d898edd
Kazu Yamamoto <kazu@iij.ad.jp>
2024-08-06 10:06:30 +0900

    fix

--- warp/bench/Parser.hs
+++ warp/bench/Parser.hs
@@ -19,7 +19,7 @@ import qualified Network.HTTP.Types as H
 import UnliftIO.Exception (impureThrow, throwIO)
 import Prelude hiding (lines)
 
-import Network.Wai.Handler.Warp.Request (headerLines)
+import Network.Wai.Handler.Warp.Request (FirstRequest (..), headerLines)
 import Network.Wai.Handler.Warp.Types
 
 #if MIN_VERSION_gauge(0, 2, 0)
